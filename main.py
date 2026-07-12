056dccdd3a19f02fe512b346eec5a0658c84857b
architpoonia09 <architpoonia09@gmail.com>
2026-07-12 17:58:15 +0530

    screen created

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-import pygame
+import pygame as pg
 from constants import *
+from logger import log_state
 
 
 def main():
-    
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    pg.init()
+    print(f"Starting Asteroids with pygame version: {pg.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        log_state()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+            
+    
+        screen.fill("black")
+
+
+        pg.display.flip()
+            
 
 if __name__ == "__main__":
     main()
